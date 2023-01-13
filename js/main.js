d4bcaf7753f22235cc8d629ac468e003d1f23e9e
Lelo2000 <58939948+Lelo2000@users.noreply.github.com>
2023-01-13 18:14:09 +0100

    spawn objects and collision works

--- js/main.js
+++ js/main.js
@@ -1,20 +1,107 @@
-console.log("hi");
+import { Object } from "./object.js";
 
-let canvas = new fabric.Canvas("canvas", { backgroundColor: "#8C8C8C" });
-canvas.setWidth(window.innerWidth);
-canvas.setHeight(window.innerHeight);
+/**@type {Map} Eine Map mit allen Objekten, welche auf dem Canvas platziert wurden*/
+const objectList = new Map();
 
-addImage("./images/green.png");
-addImage("./images/blue.png");
-addImage("./images/red.png");
-addImage("./images/yellow.png");
+$(document).ready(function () {
+  $(".button").on("mousedown", function (event) {
+    const htmlObj = event.target;
+    const objId = htmlObj.id;
+    let imgName = objId.split("-")[1];
+    addImage(`./images/${imgName}.png`, { x: 300, y: 300 }, true);
+    console.log(event.target.id);
+  });
+});
+
+const canvas = new fabric.Canvas("simulationCanvas", {
+  backgroundColor: "#8C8C8C",
+});
+setupCanvas();
 
 /**
  * Fügt ein Bild direkt zum Canvas hinzu
  * @param {string} imgURL URL des Images
+ * @param {object} position Position bei der das Bild hinzugefügt werden soll. Standard: {x: 0, y: 0}
+ * @param {boolean} center Ob das Bild um position zentriert sein soll. Standard: false
  */
-function addImage(imgURL) {
+function addImage(imgURL, position = { x: 0, y: 0 }, center = false) {
   fabric.Image.fromURL(imgURL, function (oImg) {
+    console.log(oImg);
+    if (center) {
+      oImg.top = position.y - oImg.height / 2;
+      oImg.left = position.x - oImg.width / 2;
+    } else {
+      oImg.top = position.y;
+      oImg.left = position.x;
+    }
     canvas.add(oImg);
+    canvas.setActiveObject(oImg);
+    let newObject = new Object(oImg);
+    oImg.id = newObject.id;
+    console.log(oImg);
+    objectList.set(newObject.id, newObject);
   });
 }
+
+function setupCanvas() {
+  canvas.setWidth(window.innerWidth);
+  canvas.setHeight(window.innerHeight);
+
+  canvas.on({
+    "object:moving": onChange,
+    "object:scaling": onChange,
+    "object:rotating": onChange,
+  });
+}
+
+function onChange(options) {
+  options.target.setCoords();
+  let movedObjectId = options.target.id;
+  let movedObject = objectList.get(movedObjectId);
+  let copyObjectList = new Map(objectList);
+  let collidedObjects = [];
+
+  copyObjectList.delete(movedObjectId);
+  copyObjectList.forEach((object, key) => {
+    let displayObject = object.displayObject;
+    if (options.target.intersectsWithObject(displayObject)) {
+      collidedObjects.push(displayObject.id);
+    }
+  });
+
+  collidedObjects.forEach((id) => {
+    movedObject.onCollision(id);
+  });
+
+  collidedObjects.forEach((id) => {
+    objectList.get(id).onCollision(movedObjectId);
+    copyObjectList.delete(id);
+  });
+
+  copyObjectList.forEach((object) => {
+    object.endCollision(movedObjectId);
+    movedObject.endCollision(object.id);
+  });
+
+  objectList.forEach((object) => {
+    console.log(
+      `Objects: ${object.id} Collided Objects: ${object.collidedObjectIds}`
+    );
+  });
+}
+
+// /**
+//  * Gibt die aktuelle Position der Maus relativ zum Canvas aus.
+//  */
+
+// function getMousePositionOnCanvas(event) {
+//   console.log("X: " + event.clientX + ", Y: " + event.clientY);
+//   let mouseX = event.clientX;
+//   let mouseY = event.clientY;
+//   let canvas = $("#simulationCanvas").offset();
+
+//   let returnValue = { x: mouseX, y: mouseY };
+//   if (canvas.top > mouseY) returnValue.y = 0;
+//   if (canvas.left > mouseX) returnValue.x = 0;
+//   return returnValue;
+// }
---
+++ js/object.js
@@ -0,0 +1,45 @@
+export class Object {
+  constructor(displayObject) {
+    this.id = Math.random().toFixed(4);
+    this.tags = ["object"];
+    this.displayObject = displayObject;
+    this.isCollided = false;
+    /**@type {Array} Array an Objekt Ids von den Objekten mit denen es kollidiert ist. */
+    this.collidedObjectIds = [];
+  }
+  /**
+   * Wird aufgerufen, wenn es mit einem anderen Objekt kollidiert
+   * @param collidedObjectId ID des Objektes mit dem es kollidiert ist.
+   */
+  onCollision(collidedObjectId) {
+    if (this.collidedObjectIds.indexOf(collidedObjectId) >= 0) return;
+    console.log("START Collision with: " + collidedObjectId);
+    this.collidedObjectIds.push(collidedObjectId);
+    this.updateCollisionStatus();
+  }
+
+  updateCollisionStatus() {
+    if (this.collidedObjectIds.length > 0) {
+      this.isCollided = true;
+      this.displayObject.set("opacity", 0.5);
+    } else {
+      this.isCollided = false;
+      this.displayObject.set("opacity", 1);
+    }
+  }
+
+  /**
+   * Wird aufgerufen, wenn es ein Objekt nicht mit dem Objekt kollidiert.
+   * @param removedObjectId ID des Objektes mit dem es eventuell vorher kollidiert ist.
+   */
+  endCollision(removedObjectId) {
+    if (this.collidedObjectIds.indexOf(removedObjectId) < 0) return;
+    console.log(this.id + " ENDS Collision with: " + removedObjectId);
+    this.collidedObjectIds.splice(
+      this.collidedObjectIds.indexOf(removedObjectId),
+      1
+    );
+    console.log(this.collidedObjectIds);
+    this.updateCollisionStatus();
+  }
+}

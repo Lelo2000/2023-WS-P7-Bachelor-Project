ed7c567bdadbdb9bd44bbe22334ec2b5350a7663
Lelo2000 <58939948+Lelo2000@users.noreply.github.com>
2023-03-21 14:35:16 +0100

    added new ideas icons and content

--- server/ideaManager.js
+++ server/ideaManager.js
@@ -5,22 +5,46 @@ export default class IdeaManager {
     /**@type {Map<Number, Message>} */
     this.ideas = new Map();
     this.io = io;
-    this.ideas.set(0.2837756259578277, {
-      title: "Mathilden Höhe",
-      text: "Eine Umgestaltung des Marktplatzes hin zu einer barrierefreien Umgebung ist wichtig, da dadurch alle Bürger, egal ob mit oder ohne Beeinträchtigung, gleichberechtigt an der Nutzung des Platzes teilhaben können.",
+
+    this.ideas.set(0.283775612578277, {
+      title: "Ein Brunnen für den Mathildenplatz",
+      text: `Wir wollen den Mathildenplatz attraktiver und zu einem Ort der Erholung und Entspannung zu machen. Er sorgt für 
+      eine angenehme Atmosphäre und kann zu einem neuen Treffpunkt werden. Das plätschernde Wasser hat eine beruhigende Wirkung und kann Stress abbauen. Darüber hinaus zieht ein Brunnen oft auch Vögel und andere Tiere an, was dem Platz zusätzlichen Charme verleiht. `,
       author: "Stadt",
       time: 1676134963354,
+      votes: { likes: 0, dislikes: 0 },
+      id: 0.283775612578277,
+      tags: [],
+      comments: [],
+      markerPoint: { lat: 49.874881, lng: 8.650426 },
+      status: "active",
+      projectId: 3,
+    });
+    this.ideas.set(0.2837756259578247, {
+      title: "Fahrradfreundlichere Landwehrstraße",
+      text: `Um die Landwehrstraße fahrradfreundlicher zu gestalten, könnten folgende Maßnahmen in Angriff genommen werden:
+      <ul>
+      <li>Der Ausbau der Straße zu einer fahrradfreundlichen 30er Zone, um die Geschwindigkeit des Autoverkehrs zu reduzieren und die Sicherheit von Radfahrern zu erhöhen.</li>
+      <li>Die Einrichtung von Fahrradspuren oder -wegen, um Radfahrern einen sicheren Bereich zu bieten.</li>
+      <li>Der Bau von Fahrradboxen oder Abstellanlagen, um das Abstellen von Fahrrädern zu erleichtern.</li>
+      <li>Die Installation von Verkehrsberuhigungsmaßnahmen, wie beispielsweise Verkehrsinseln, um den Verkehr zu verlangsamen und die Sicherheit zu erhöhen.</li>
+      <li>Eine verbesserte Beleuchtung, um die Sichtbarkeit von Radfahrern bei Dunkelheit zu erhöhen.</li>
+      <li>Die Durchführung von Verkehrsschulungen und -kampagnen, um das Bewusstsein für die Sicherheit von Radfahrern zu erhöhen und das Verhalten von Autofahrern zu verbessern.</li>
+      </ul>
+      Durch die Umsetzung dieser Maßnahmen könnte die Landwehrstraße sicherer und attraktiver für Radfahrer werden, was letztendlich dazu beitragen könnte, den Fahrradverkehr zu erhöhen und die Umweltbelastung zu reduzieren.`,
+      author: "Stadt",
+      time: 1671455978,
       votes: { likes: 0, dislikes: 0 },
       id: 0.2837756259578247,
       tags: [],
       comments: [],
-      markerPoint: { lat: 49.8759698310847, lng: 8.649415969848635 },
-      status: "active",
+      markerPoint: { lat: 49.87868070335318, lng: 8.640192425232668 },
+      status: "idea",
       projectId: 1,
     });
 
-    this.ideas.set(0.2833200756259578277, {
-      title: "Platz Höhe",
+    this.ideas.set(0.2837756259578277, {
+      title: "Umgestaltung des Marktplatzes",
       text: "Eine Umgestaltung des Marktplatzes hin zu einer barrierefreien Umgebung ist wichtig, da dadurch alle Bürger, egal ob mit oder ohne Beeinträchtigung, gleichberechtigt an der Nutzung des Platzes teilhaben können.",
       author: "Stadt",
       time: 1676134963354,
@@ -28,23 +52,9 @@ export default class IdeaManager {
       id: 0.2837756259578277,
       tags: [],
       comments: [],
-      markerPoint: { lat: 49.8659698310847, lng: 8.639415969848635 },
-      status: "active",
-      projectId: 2,
-    });
-
-    this.ideas.set(0.283775612578277, {
-      title: "Luisenplatz",
-      text: "Eine Umgestaltung des Marktplatzes hin zu einer barrierefreien Umgebung ist wichtig, da dadurch alle Bürger, egal ob mit oder ohne Beeinträchtigung, gleichberechtigt an der Nutzung des Platzes teilhaben können.",
-      author: "Stadt",
-      time: 1676134963354,
-      votes: { likes: 0, dislikes: 0 },
-      id: 0.283775612578277,
-      tags: [],
-      comments: [],
-      markerPoint: { lat: 49.8769798310847, lng: 8.659515969848635 },
+      markerPoint: { lat: 49.87298709873493, lng: 8.655710091419401 },
       status: "finished",
-      projectId: 3,
+      projectId: 2,
     });
   }
 
